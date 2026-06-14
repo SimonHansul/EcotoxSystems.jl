3b9b29a686b6c261d61afccb2d36127d4bb02356
Simon Hansul <hansul@gaiac-eco.de>
2026-06-14 23:35:10 +0200

    add runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,10 @@
+using Test
+
+include("test00_setup.jl")
+include("Aqua.jl")
+include("test01_defaults.jl")
+include("test02_food.jl")
+include("test03_toxicity.jl")
+include("test04_temperature.jl")
+include("test05_mixtures.jl")
+include("test06_ibm_defparams.jl")
